2ff1efc307f678ce71a036f2f5c7c85cccc66c87
Attila Kun <kun.attila.hun@gmail.com>
2019-03-17 02:16:51 +0000

    Reading reflog.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use git2::Repository;
+
+fn get_reflog() -> git2::Reflog {
+
+    let repo = match Repository::open("/home/attila/dev/projects/TypeScript") {
+        Ok(repo) => repo,
+        Err(e) => panic!("failed to open: {}", e),
+    };
+
+    match repo.reflog("HEAD") {
+        Ok(reflog) => reflog,
+        Err(e) => panic!("could not open reflog: {}", e)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let reflog = get_reflog();
+    println!("reflog length: {}", reflog.len());
+
+    for entry in reflog.iter() {
+        match entry.message() {
+            Some(msg) => println!("entry {}", msg),
+            None => ()
+        }        
+    }
 }
